07e2270434b333c44884484d93cbc8786a67e124
13517012 Johanes <13517012@std.stei.itb.ac.id>
2021-04-19 02:42:59 +0000

    Basic player movement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerController : MonoBehaviour
+    {
+        public float linearForce = 500f;
+        public float maxLinearSpeed = 100f;
+        public float angularForce = 1000f;
+        public float maxAngularSpeed = 1000f;
+
+        private Rigidbody2D _rb;
+        private Vector2 _movementInput = Vector2.zero;
+        private float _rotationsInput;
+    
+        #region unity callback
+        void Start()
+        {
+            _rb = gameObject.GetComponent<Rigidbody2D>();
+            var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            spriteRenderer.color = Random.ColorHSV();
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.AddForce(_movementInput * (linearForce * Time.fixedDeltaTime));
+            var velocity = _rb.velocity;
+            _rb.velocity = velocity.normalized * Mathf.Clamp(velocity.magnitude, 0, maxLinearSpeed);
+            _rb.AddTorque(_rotationsInput * angularForce * Time.fixedDeltaTime);
+            _rb.angularVelocity = Mathf.Clamp(_rb.angularVelocity, -maxAngularSpeed, maxAngularSpeed);
+        }
+
+        #endregion
+
+        #region public callback
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            _movementInput = context.ReadValue<Vector2>();
+        }
+
+        public void OnRotate(InputAction.CallbackContext context)
+        {
+            _rotationsInput = context.ReadValue<float>();
+        }
+
+        #endregion
+    }
+}
