8248e9e543f240053da063837b0bca2a4c509467
Steve Immanuel <iam.steve.immanuel@gmail.com>
2021-05-04 13:39:38 +0000

    Integrate tower script with ruble

--- Assets/Scripts/Tower/TowerHealth.cs
+++ Assets/Scripts/Tower/TowerHealth.cs
@@ -3,18 +3,27 @@ using UnityEngine;
 
 namespace Tower
 {
+    [RequireComponent(typeof(SpriteRenderer))]
     public class TowerHealth : MonoBehaviour
     {
         public float health = 1000f;
-        private float maxSliceHealth;
-
         public float damageMultiplier = 10f;
+        public GameObject rubleParticlePrefab;
+
+        private float maxSliceHealth;
+        private SpriteRenderer spriteRenderer;
+
         #region unity callback
+
+        private void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
 
         private void Start()
         {
             // Define how max of slice health point of tower
-            maxSliceHealth = health / 5;            
+            maxSliceHealth = health / 5;
         }
 
         private void OnCollisionEnter2D(Collision2D other)
@@ -26,6 +35,19 @@ namespace Tower
             // Know which body of tower got hit
             ContactPoint2D contact = other.contacts[0];
             Debug.Log(contact.collider.name + " hit " + contact.otherCollider.name);
+
+            Debug.Log(contact.normalImpulse);
+            if (contact.normalImpulse > 2000f)
+            {
+                Quaternion rotation = Quaternion.LookRotation(contact.normal);
+                var ruble = Instantiate(rubleParticlePrefab, contact.point, rotation);
+                ParticleSystem rubleParticle = ruble.GetComponent<ParticleSystem>();
+                var mainModule = rubleParticle.main;
+                mainModule.startColor = spriteRenderer.color;
+                var emissionModule = rubleParticle.emission;
+                emissionModule.rateOverTime = contact.normalImpulse / 8000 * mainModule.maxParticles;
+                Destroy(ruble, 1f);
+            }
 
             //Calculate the damage based on magnitude
             float damage;
