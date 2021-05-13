f6f7490f89a1c57c39ae871756c11f3d0fbbe34b
13517012 Johanes <13517012@std.stei.itb.ac.id>
2021-05-13 17:16:29 +0000

    Add simple timer

---
+++ Assets/Scripts/Manager/Timer.cs
@@ -0,0 +1,72 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace Manager
+{
+    public class Timer : MonoBehaviour
+    {
+        public float timeRemaining = 3 * 60;
+        public string monoSpacingSize = "30";
+        [SerializeField] private bool timerStarted;
+
+        private TMP_Text _timerText;
+        public Action OnTimerEnd;
+
+        #region private function
+
+        private void DisplayTimer()
+        {
+            var minutes = Math.Floor(timeRemaining / 60);
+            var seconds = Math.Floor(timeRemaining % 60);
+            _timerText.text = $"<mspace=mspace={monoSpacingSize}>{minutes:00}:{seconds:00}</mspace>";
+        }
+
+        #endregion
+
+        #region public function
+
+        public void StartTimer()
+        {
+            timerStarted = true;
+        }
+
+        public void StopTimer()
+        {
+            timerStarted = false;
+        }
+
+        #endregion
+
+        #region unity callback
+
+        private void Awake()
+        {
+            _timerText = GetComponent<TMP_Text>();
+        }
+
+        private void Start()
+        {
+            DisplayTimer();
+        }
+
+        private void Update()
+        {
+            if (!timerStarted) return;
+
+            if (timeRemaining > 0) timeRemaining -= Time.deltaTime;
+
+            timeRemaining = Math.Max(timeRemaining, 0);
+
+            DisplayTimer();
+
+            if (timeRemaining == 0)
+            {
+                timerStarted = false;
+                OnTimerEnd?.Invoke();
+            }
+        }
+
+        #endregion
+    }
+}
