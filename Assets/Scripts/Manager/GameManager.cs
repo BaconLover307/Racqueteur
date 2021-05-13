f1c1a7c750e18bd5a131b690f5abe29e050a0a79
Daniel Riyanto <13518075@std.stei.itb.ac.id>
2021-05-13 09:28:50 +0000

    Add key bindings option (stuck applying to KeyboardSplitter)

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,9 +9,43 @@ public class GameManager : MonoBehaviour
     public ArenaLight arenaLight;
     public TowerLight[] towerLights;
     public BorderLight[] borderLights;
+    private KeyboardSplitter keyboardSplitter;
 
     void Start()
     {
+        keyboardSplitter = GetComponent<KeyboardSplitter>();
+
+        // Set key binding from PlayerPrefs
+        // TODO: Change parameter kedua dari PlayerPrefs.GetString ke variable remapping yang ada di KeyboardSplitter
+        //       & assign variabel remapping dengan keyCode-keyCode di bawah
+        KeyCode keyUp1 = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Up1", "W"));
+        KeyCode keyDown1 = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Down1", "S"));
+        KeyCode keyLeft1 = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Left1", "A"));
+        KeyCode keyRight1 = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Right1", "D"));
+        KeyCode keyCW1 = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("CW1", "V"));
+        KeyCode keyCCW1 = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("CCW1", "N"));
+
+        KeyCode keyUp2 = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Up2", "I"));
+        KeyCode keyDown2 = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Down2", "K"));
+        KeyCode keyLeft2 = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Left2", "J"));
+        KeyCode keyRight2 = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Right2", "L"));
+        KeyCode keyCW2 = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("CW2", "LeftArrow"));
+        KeyCode keyCCW2 = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("CCW2", "RightArrow"));
+
+        Debug.Log(keyUp1);
+        Debug.Log(keyDown1);
+        Debug.Log(keyLeft1);
+        Debug.Log(keyRight1);
+        Debug.Log(keyCW1);
+        Debug.Log(keyCCW1);
+
+        Debug.Log(keyUp2);
+        Debug.Log(keyDown2);
+        Debug.Log(keyLeft2);
+        Debug.Log(keyRight2);
+        Debug.Log(keyCW2);
+        Debug.Log(keyCCW2);
+
         StartCoroutine(TurnOnLights());
     }
 
--- Assets/Scripts/Manager/Options.cs
+++ Assets/Scripts/Manager/Options.cs
@@ -2,11 +2,90 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Options : MonoBehaviour
 {
+    private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+
+    public TextMeshProUGUI up1, down1, left1, right1, cw1, ccw1;
+    public TextMeshProUGUI up2, down2, left2, right2, cw2, ccw2;
+
+    private GameObject currentKey;
+
+    public void Start()
+    {
+        keys.Add("Up1", (KeyCode)System.Enum.Parse(typeof(KeyCode),PlayerPrefs.GetString("Up1", "W")));
+        keys.Add("Down1", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Down1", "S")));
+        keys.Add("Left1", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Left1", "A")));
+        keys.Add("Right1", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Right1", "D")));
+        keys.Add("CW1", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("CW1", "V")));
+        keys.Add("CCW1", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("CCW1", "N")));
+
+        keys.Add("Up2", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Up2", "I")));
+        keys.Add("Down2", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Down2", "K")));
+        keys.Add("Left2", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Left2", "J")));
+        keys.Add("Right2", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Right2", "L")));
+        keys.Add("CW2", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("CW2", "LeftArrow")));
+        keys.Add("CCW2", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("CCW2", "RightArrow")));
+
+
+        up1.text = keys["Up1"].ToString();
+        down1.text = keys["Down1"].ToString();
+        left1.text = keys["Left1"].ToString();
+        right1.text = keys["Right1"].ToString();
+        cw1.text = keys["CW1"].ToString();
+        ccw1.text = keys["CCW1"].ToString();
+
+        up2.text = keys["Up2"].ToString();
+        down2.text = keys["Down2"].ToString();
+        left2.text = keys["Left2"].ToString();
+        right2.text = keys["Right2"].ToString();
+        cw2.text = keys["CW2"].ToString();
+        ccw2.text = keys["CCW2"].ToString();
+    }
+
     public void BackScene()
     {
         SceneManager.LoadScene("MainMenuScene");
+    }
+
+    private void OnGUI()
+    {
+        if (currentKey != null)
+        {
+            Event e = Event.current;
+
+            if (e.isKey)
+            {
+                string currentName = currentKey.transform.GetChild(0).GetComponent<TextMeshProUGUI>().name;
+
+                keys[currentName] = e.keyCode;
+                Debug.Log(currentName);
+                currentKey.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = e.keyCode.ToString();
+
+                // Save the key binding to PlayerPrefs
+                PlayerPrefs.SetString(currentName, e.keyCode.ToString());
+                PlayerPrefs.Save();
+
+                currentKey = null;
+            }
+        }
+    }
+
+    public void ChangeKey(GameObject clicked)
+    {
+        currentKey = clicked;
+    }
+
+    // Save all key bindings when pressed Save
+    public void saveKeys()
+    {
+        foreach(var key in keys)
+        {
+            PlayerPrefs.SetString(key.Key, key.Value.ToString());
+        }
+
+        PlayerPrefs.Save();
     }
 }
