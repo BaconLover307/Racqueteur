6f91fc40530581d5bd5ddd82163a61ae4182ea21
Steve Immanuel <iam.steve.immanuel@gmail.com>
2021-05-14 05:34:42 +0000

    Add chromatic abbreviation and vignette

--- Assets/Scripts/Camera/CameraShake.cs
+++ Assets/Scripts/Camera/CameraShake.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class CameraShake : MonoBehaviour
 {
     public float shakeDuration;
     public float maxAmplitude;
+    public VolumeProfile globalVolume;
 
     private CinemachineVirtualCamera vCam;
     private CinemachineBasicMultiChannelPerlin noise;
@@ -26,9 +29,22 @@ public class CameraShake : MonoBehaviour
 
     IEnumerator ProcessShake(float amplitude, float frequency, float duration)
     {
+        ChromaticAberration caSetting;
+        globalVolume.TryGet(out caSetting);
+
+        caSetting.intensity.Override(1);
         noise.m_AmplitudeGain = amplitude;
         noise.m_FrequencyGain = frequency;
-        yield return new WaitForSeconds(shakeDuration);
+
+        float currentTime = 0f;
+        while (currentTime < shakeDuration)
+        {
+            caSetting.intensity.Override(1 - currentTime / shakeDuration);
+            currentTime += Time.deltaTime;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        caSetting.intensity.Override(0);
         noise.m_AmplitudeGain = 0;
         noise.m_FrequencyGain = 0;
     }
