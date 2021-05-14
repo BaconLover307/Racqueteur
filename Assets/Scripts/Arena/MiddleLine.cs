eced90b963eb0e32a4eea70fdb30cc6bb5ef1762
Jovan Kresnadi <kresnadi30@gmail.com>
2021-05-14 16:24:11 +0000

    Fixed middle line

--- Assets/Scripts/Arena/MiddleLine.cs
+++ Assets/Scripts/Arena/MiddleLine.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class MiddleLine : MonoBehaviour
 {
-    // Filter all collider which collider will be abandoned and considered
-    void OnCollisionEnter2D(Collision2D collision)
+    public void Start()
     {
-        if (collision.gameObject.tag == "Ball")
+        GameObject ballObject = GameObject.FindGameObjectWithTag("Ball");
+        Physics2D.IgnoreCollision(ballObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+
+        GameObject[] playerObjects = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject playerObject in playerObjects)
         {
-            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+            Collider2D[] colliders = playerObject.GetComponents<Collider2D>();
+            foreach (Collider2D collider in colliders)
+            {
+                if (collider.GetType() == typeof(BoxCollider2D)
+                || collider.GetType() == typeof(PolygonCollider2D)) 
+                {
+                    Physics2D.IgnoreCollision(collider, GetComponent<Collider2D>());
+                }
+            }
         }
-        else if ((collision.gameObject.tag == "Player") &&
-            (collision.gameObject.GetComponent<Collider2D>().GetType() == typeof(BoxCollider2D)
-            || collision.gameObject.GetComponent<Collider2D>().GetType() == typeof(PolygonCollider2D))) 
-        {
-            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
-        }
+        
     }
 }
