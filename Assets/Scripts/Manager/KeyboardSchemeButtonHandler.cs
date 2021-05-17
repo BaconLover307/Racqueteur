8120847f30d315cdd889f6fa7eb6c4e3e5b4bf9b
13517012 Johanes <13517012@std.stei.itb.ac.id>
2021-05-17 18:17:15 +0000

    Redesign control options with layout

--- Assets/Scripts/Manager/KeyboardSchemeButtonHandler.cs
+++ Assets/Scripts/Manager/KeyboardSchemeButtonHandler.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 namespace Manager
@@ -7,8 +8,11 @@ namespace Manager
     public class KeyboardSchemeButtonHandler : MonoBehaviour
     {
         public Button[] buttons;
-        public List<string> names; 
+        public List<string> names;
         public int playerIdx;
+        public InputActionAsset playerControls;
+
+        private Dictionary<string, List<string>> _defaultControls;
 
         private void Start()
         {
@@ -18,12 +22,14 @@ namespace Manager
                 buttons[i].onClick.AddListener(delegate { ChangeScheme(idx); });
             }
 
+            ParseInputActionAsset();
+
             if (DeviceMap.PlayerDevices != null)
             {
                 // not default options
                 var (_, scheme) = DeviceMap.PlayerDevices[playerIdx];
                 if (!scheme.StartsWith("Keyboard")) return;
-                
+
                 foreach (var button in buttons) button.interactable = true;
                 var idx = names.IndexOf(scheme);
                 buttons[idx].interactable = false;
@@ -36,6 +42,26 @@ namespace Manager
 
             buttons[idx].interactable = false;
             DeviceManager.Instance.OnChangeScheme(playerIdx + "," + names[idx]);
+        }
+
+        private void ParseInputActionAsset()
+        {
+            _defaultControls = new Dictionary<string, List<string>>();
+            foreach (var inputBinding in playerControls.FindActionMap("Player").FindAction("Movement").bindings)
+            {
+                if (inputBinding.groups.Equals("")) continue;
+
+                if (inputBinding.groups.Equals("Gamepad"))
+                {
+                    _defaultControls["Gamepad"] = new List<string> {inputBinding.path.Split('/')[1]};
+                }
+                else
+                {
+                    if (!_defaultControls.ContainsKey(inputBinding.groups))
+                        _defaultControls[inputBinding.groups] = new List<string>();
+                    _defaultControls[inputBinding.groups].Add(inputBinding.path.Split('/')[1]);
+                }
+            }
         }
     }
 }
