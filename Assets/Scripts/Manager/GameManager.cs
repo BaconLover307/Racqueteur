0f9d01bc611826d44928c7e57bba3812c6e51229
13518056 Michael Hans <13518056@std.stei.itb.ac.id>
2021-05-18 05:22:02 +0000

    Edit Announcement Delay

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -142,13 +142,14 @@ public class GameManager : MonoBehaviour
         if (timeRemaining == 30)
         {
             _audioManager.PlayAnnounce(ann30sSFX);
+            notificationDisplay.text = "30 Seconds Remaining";
         }
         if (timeRemaining == 60)
         {
             _audioManager.PlayAnnounce(ann60sSFX);
+            notificationDisplay.text = "1 Minute Remaining";
         }
 
-        notificationDisplay.text = timeRemaining.ToString() + " Seconds Remaining";
         StartCoroutine(ShowTimeNotification());
     }
 
@@ -156,7 +157,7 @@ public class GameManager : MonoBehaviour
     {
         notificationDisplay.gameObject.SetActive(true);
         notificationDisplay.gameObject.GetComponent<Animator>().Play("Announcement");
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(4.0f);
         notificationDisplay.gameObject.SetActive(false);
     }
 
